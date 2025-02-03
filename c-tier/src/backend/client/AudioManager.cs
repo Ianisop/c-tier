8eba15086f30bc7d1d7c950460cf5cb7d569f381
SomethingFishy <69714456+Ianisop@users.noreply.github.com>
2025-02-03 19:36:19 +0100

    wip audio changes

--- c-tier/src/backend/client/AudioManager.cs
+++ c-tier/src/backend/client/AudioManager.cs
@@ -12,20 +12,27 @@ namespace c_tier.src.backend.client
     public static class AudioManager
     {
         public static PvRecorder recorder;
+        public static PvSpeaker speaker;
+        public static int sampleRate = 22050;
+        public static int bitsPerSample = 16;
         public static int frameLength = 512; // 512 default
         public static short[] inputAudioFrames;
         public static Dictionary<int, string> inputDevices = new Dictionary<int, string>();
         public static Dictionary<int, string> outputDevices = new Dictionary<int, string>();
 
 
-
+        /// <summary>
+        /// Initializes the audio manager devices and creates a recording devices
+        /// </summary>
         public static void Init()
         {
             FindDevices();
-            Console.WriteLine("Finding devices...");
             recorder = PvRecorder.Create(frameLength, 0);
-            Console.WriteLine("Recorder open with device");
-
+            speaker = new PvSpeaker(
+                sampleRate:sampleRate,
+                bitsPerSample:bitsPerSample
+                );
+          
         }
 
         /// <summary>
@@ -37,8 +44,18 @@ namespace c_tier.src.backend.client
             while (recorder.IsRecording)
             {
                 inputAudioFrames = recorder.Read();
-                
             }
+        }
+
+        public static void Play(byte[] samples)
+        {
+            speaker.Start();
+            while(speaker.IsStarted)
+            {
+                int writtenSamples = speaker.Write(samples); // write to the audio buffer
+                int flushedLength = speaker.Flush(); // this is what plays the audio
+            }
+            speaker.Stop();
         }
 
         /// <summary>
@@ -47,10 +64,16 @@ namespace c_tier.src.backend.client
         public static void FindDevices()
         {
             string[] devices = PvRecorder.GetAvailableDevices();
-        
+            
             for(int i = 0; i < devices.Length; i++)
             {
                 inputDevices.Add(i, devices[i]);
+            }
+
+            string[] odevices = PvSpeaker.GetAvailableDevices();
+            for (int i = 0; i < odevices.Length; i++)
+            {
+                outputDevices.Add(i, odevices[i]);
             }
         }
 
